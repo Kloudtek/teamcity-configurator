09ab1bd2c887dcc3bb5cb291bd22e0d6ffbe3cd3
Yannick Menager <ymenager@kloudtek.com>
2015-03-31 19:00:38 +0100

    revert version to snapshot

--- src/main/java/com/kloudtek/teamcityconfigurator/ConfigureTeamcity.java
+++ src/main/java/com/kloudtek/teamcityconfigurator/ConfigureTeamcity.java
@@ -9,7 +9,6 @@ import com.gargoylesoftware.htmlunit.Page;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.*;
 
-import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -26,9 +25,11 @@ public class ConfigureTeamcity {
     private int timeout = 10000;
     @Parameter(names = {"-wt", "--wait-timeout"}, description = "How long to wait for server to be ready")
     private long waitTimeout = 3 * 60000L;
-    @Parameter(names = {"-n"}, description = "Username for the admin account to be created on teamcity",required = true)
+    @Parameter(names = {"-st", "--setup-timeout"}, description = "How long to wait for successful server setup")
+    private long setupTimeout = 2 * 60000L;
+    @Parameter(names = {"-n"}, description = "Username for the admin account to be created on teamcity", required = true)
     private String adminUsername;
-    @Parameter(names = {"-p"}, description = "Password for the admin account to be created on teamcity",required = true)
+    @Parameter(names = {"-p"}, description = "Password for the admin account to be created on teamcity", required = true)
     private String adminPassword;
     private boolean debug = false;
 
@@ -40,7 +41,7 @@ public class ConfigureTeamcity {
         WebClient webClient = new WebClient();
         webClient.getOptions().setTimeout(timeout);
         Page page;
-        for (;;) {
+        for (; ; ) {
             try {
                 page = webClient.getPage(serverUrl);
                 if (validSetupPage(page)) {
@@ -57,33 +58,34 @@ public class ConfigureTeamcity {
                 sleep(2500L);
             }
         }
-        try {
-            boolean completed = false;
-            boolean adminSetup = false;
-            while (!completed) {
+        boolean completed = false;
+        boolean adminSetup = false;
+        long setupTimelimt = System.currentTimeMillis() + setupTimeout;
+        while (!completed) {
+            try {
                 String url = page.getUrl().toString();
-                if( url.endsWith(AGREEMENT) ) {
+                if (url.endsWith(AGREEMENT)) {
                     System.out.println("Accepting license agreement");
                     ((HtmlCheckBoxInput) ((HtmlPage) page).getElementById("accept")).setChecked(true);
                     ((HtmlCheckBoxInput) ((HtmlPage) page).getElementById("sendUsageStatistics")).setChecked(false);
                     page = ((HtmlSubmitInput) ((HtmlPage) page).getElementByName("Continue")).click();
                     validatePageUrl(page, SETUP_ADMIN);
-                } else if( url.endsWith(SETUP_ADMIN) ) {
-                    if( ! adminSetup ) {
+                } else if (url.endsWith(SETUP_ADMIN)) {
+                    if (!adminSetup) {
                         System.out.println("Creating admin user");
                         adminSetup = true;
                         webClient.getOptions().setJavaScriptEnabled(true);
                         page = webClient.getPage(url);
                     } else {
                         DomElement usernameInputField = ((HtmlPage) page).getElementById("input_teamcityUsername");
-                        if( usernameInputField == null ) {
+                        if (usernameInputField == null) {
                             page = webClient.getPage(serverUrl);
                         } else {
                             ((HtmlTextInput) usernameInputField).type(adminUsername);
                             ((HtmlPasswordInput) ((HtmlPage) page).getElementById("password1")).type(adminPassword);
                             ((HtmlPasswordInput) ((HtmlPage) page).getElementById("retypedPassword")).type(adminPassword);
                             page = ((HtmlSubmitInput) ((HtmlPage) page).getFirstByXPath("//input[@value='Create Account']")).click();
-                            if( page.getUrl().toString().endsWith(LOGIN) ) {
+                            if (page.getUrl().toString().endsWith(LOGIN)) {
                                 completed = true;
                             } else {
                                 sleep(1000L);
@@ -92,18 +94,25 @@ public class ConfigureTeamcity {
                         }
                     }
                 } else {
-                    if( adminSetup && pageIsProjectPage(page) ) {
+                    if (adminSetup && pageIsProjectPage(page)) {
                         completed = true;
                     } else {
                         System.out.println(((HtmlPage) page).asXml());
-                        throw new SetupException("Unexpected page: "+ url);
+                        throw new SetupException("Unexpected page: " + url);
+                    }
+                }
+            } catch (Exception e) {
+                if (System.currentTimeMillis() > setupTimelimt) {
+                    throw new SetupException(e);
+                } else {
+                    System.out.println("Error occured setting up, retrying: " + e.getMessage());
+                    if (debug) {
+                        e.printStackTrace();
                     }
                 }
             }
-            System.out.println("Teamcity server configuration completed");
-        } catch (IOException e) {
-            throw new SetupException(e);
         }
+        System.out.println("Teamcity server configuration completed");
     }
 
     private boolean pageIsProjectPage(Page page) {
